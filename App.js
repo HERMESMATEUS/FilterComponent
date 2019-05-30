38f4a894cad522d595ff14e2bfbd5643fb26835e
hermesmateus <drakhermes@hotmail.com>
2019-05-30 15:30:25 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * HERMESMATEUS
+ *
+ */
+
+import React, { Component } from "react";
+import { View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return <View style={{ flex: 1 }} />;
+  }
+}
---
+++ FilterComponent/Filter.screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import Styles from './Styles';
+
+export default class FilterScreen {
+  render() {
+    const { } = this.props;
+    return (
+      <View style={{ flex: 1 }}>
+
+      </View>
+    );
+  }
+}
